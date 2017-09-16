9310491abb0a856fe0dffe9defc02c43038c8b13
Cristiano Nunes <cfgnunes@gmail.com>
2017-09-16 12:01:35 -0300

    Refatoração de código

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import interpolacao
 import polinomios
 import raizes
 
+def debug(variable):
+    print (variable, "=", eval(variable))
+
 # Método da Bisseção (encontrar raízes de uma equação)
 #   Vantagens:
 #       É um método confiável com convergência garantida;
@@ -19,14 +22,14 @@ tol = 10 ** -5
 iter_max = 100
 a = 1.0
 b = 2.0
-print("tol:", tol)
-print("iter_max:", iter_max)
-print("a:", a)
-print("b:", b)
+debug("tol")
+debug("iter_max")
+debug("a")
+debug("b")
 [raiz, iter, cond_erro] = raizes.bissecao(funcao, a, b, tol, iter_max)
-print("raiz:", raiz)
-print("iter:", iter)
-print("condErro:", cond_erro)
+debug("raiz")
+debug("iter")
+debug("cond_erro")
 
 # Método da Secante (encontrar raízes de uma equação)
 #   Vantagens:
@@ -41,49 +44,49 @@ tol = 10 ** -5
 iter_max = 100
 a = 1.0
 b = 2.0
-print("tol:", tol)
-print("iter_max:", iter_max)
-print("a:", a)
-print("b:", b)
+debug("tol")
+debug("iter_max")
+debug("a")
+debug("b")
 [raiz, iter, cond_erro] = raizes.secante(funcao, a, b, tol, iter_max)
-print("raiz:", raiz)
-print("iter:", iter)
-print("condErro:", cond_erro)
+debug("raiz")
+debug("iter")
+debug("cond_erro")
 
 print("\n> Executando o método de Lagrange:")
 x = np.array([2, 11 / 4, 4])
 y = np.array([1 / 2, 4 / 11, 1 / 4])
 x_int = 3
-print("x:", x)
-print("y:", y)
-print("x_int:", x_int)
+debug("x")
+debug("y")
+debug("x_int")
 [y_int] = interpolacao.lagrange(x, y, x_int)
-print("y_int:", y_int)
+debug("y_int")
 
 print("\n> Executando o método de Neville:")
 x = np.array([1.0, 1.3, 1.6, 1.9, 2.2])
 y = np.array([0.7651977, 0.6200860, 0.4554022, 0.2818186, 0.1103623])
 x_int = 1.5
-print("x:", x)
-print("y:", y)
-print("x_int:", x_int)
+debug("x")
+debug("y")
+debug("x_int")
 [y_int, q] = interpolacao.neville(x, y, x_int)
-print("y_int:", y_int)
-print("q:\n", q)
+debug("y_int")
+debug("q")
 
 print("\n> Executando o método de Briot-Ruffini:")
 a = np.array([2, 0, -3, 3, -4])
 raiz = -2
-print("a:", a)
-print("raiz:", raiz)
+debug("a")
+debug("raiz")
 [b, resto] = polinomios.briot_ruffini(a, raiz)
-print("b:", b)
-print("resto:", resto)
+debug("b")
+debug("resto")
 
 print("\n> Executando o método de Diferenças Divididas de Newton")
 x = np.array([1.0, 1.3, 1.6, 1.9, 2.2])
 y = np.array([0.7651977, 0.6200860, 0.4554022, 0.2818186, 0.1103623])
-print("x:", x)
-print("y:", y)
+debug("x")
+debug("y")
 [f] = polinomios.diferencas_divididas_newton(x, y)
-print("f:", f)
+debug("f")
